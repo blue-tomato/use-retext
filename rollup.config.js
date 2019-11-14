2e272bd3b91dcf0aa7338c177042b5b786ba12a7
Manuel Penaloza <manuel.penaloza@gmx.at>
2019-11-14 09:30:33 +0100

    add base bundling

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import babel from 'rollup-plugin-babel';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export default (async() => ({
+  input: './index.js',
+  output: {
+      file: './build/bundle.min.js',
+      format: 'umd',
+      name: 'bundle'
+  },
+  plugins: [
+    isProduction && (await import('rollup-plugin-terser')).terser()
+    // babel({
+    //     exclude: 'node_modules/**'
+    // })
+  ]
+}))()
